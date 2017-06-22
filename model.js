fcd34d1dd428b4a48422e10a6575d0aedff77ea5
gggoe <gggoe@qq.com>
2017-06-22 13:28:28 +0800

    修改数据库名称

--- model.js
+++ model.js
@@ -2,7 +2,7 @@
 let mongoose = require('mongoose');
 mongoose.Promise = Promise;
 //2. 连接数据
-mongoose.connect('mongodb://127.0.0.1/char');
+mongoose.connect('mongodb://127.0.0.1/chat');
 
 // 3.定义Schema
 let MessageSchema = new mongoose.Schema({
